831b7c3ed0a37999b693516ea6ed6f2c5c089553
Luke Niland <luke.niland@pushdr.com>
2021-06-26 09:46:05 +0100

    Created donate and view donation status components

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-# algofundme
+# AlgoFundMe
+
+Simple demo project to show how to create a simple go fund me donation style web application using the algorand blockchain.
+
+The project is a Vue2 project that uses the algosdk sdk and the purestake api to talk to the algo test net and will require you to have an API key created on purestake. 
+
+Head over to https://developer.purestake.io/ to create an account and get your key.
+
+In order for the project to work you will then need to create 2 accounts on the algo test net, one that will be the donation account (receiver) and one that will be the donator account (giver)
+
+Set these two accounts up, and make a note of the pass phrases and wallet addresses for them. Pick one account to be the donator account and head over to the https://bank.testnet.algorand.network/ page where you can top the account up with algos.
+
+## Updating the source
+
+Once you have these 2 accounts created and their details, along with funds in the donator account you need to update the components with these details so the demo will work
+
+/components/Donate.Vue 
+- [YourAPIKeyHere] needs to be the API key from purestake
+- [YourReciveAddressHere] needs to be the wallet address where you want to send the donated funds to. It should be a long string of letters and numbers
+- [YourSendingPassPhraseHere] needs to be the pass phrase of the account you are going to send funds from, the same one you added funds to on the test net. This will be a string of words
+
+/components/FundingProgress.Vue
+- [YourAPIKeyHere] needs to be the API key from purestake
+- [YourDonationWalletPassPhraseHere] needs to be the pass phrase of the account you are donating to. This will be a string of words
+
+Once these deatails are filled in you should be able to send using the donate page, and see the progress towards the goad on the progress page
 
 ## Project setup
 ```
@@ -10,15 +35,4 @@ npm install
 npm run serve
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+Please not this is just a demo project, and has not been audited by any entity. Please dont use this code in production!
---
+++ src/components/Donate.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+      Your account passphrase (in production you would just connect your wallet to the application)<br/>
+      <input v-model="sendingWalletPassphrase" placeholder="passphrase"><br/>
+      Amount of micoAlgos to donate<br/>
+      <input v-model="donationAmount" placeholder="amount">
+      <button @click="donate()">Donate</button>
+      <p>
+        {{confirmationStatus}}
+      </p>
+  </div>
+</template>
+
+<script>
+
+const apiKey = "[YourAPIKeyHere]"
+const algosdk = require("algosdk")
+const baseServer = "https://testnet-algorand.api.purestake.io/ps2"
+const port = ""
+const token = {
+  "X-API-Key": apiKey
+}
+const algodClient = new algosdk.Algodv2(token, baseServer, port)
+
+export default {
+data: function() {
+      return {
+        reciveAddress: '[YourReciveAddressHere]',
+        confirmationStatus: 'Waiting for donation...',
+        sendingWalletPassphrase: '[YourSendingPassPhraseHere]',
+        donationAmount: 1000000,
+      }
+  },
+  methods: {
+    async donate() {
+      
+      let account = algosdk.mnemonicToSecretKey(this.sendingWalletPassphrase)
+      let params = await algodClient.getTransactionParams().do()
+      let note = algosdk.encodeObj("My donation")
+
+      let txn = algosdk.makePaymentTxnWithSuggestedParams(account.addr,this.reciveAddress, parseInt(this.donationAmount), undefined, note, params)
+
+      let signedTxn = txn.signTxn(account.sk);
+      console.log(signedTxn)
+      let txId = txn.txID().toString();
+      this.confirmationStatus = 'Submiting donation please wait for confirmation...'
+
+      await algodClient.sendRawTransaction(signedTxn).do();
+      let confirmedTxn = await this.waitForConfirmation(algodClient, txId, 4);
+
+      this.confirmationStatus = "Transaction " + txId + " confirmed in round " + confirmedTxn["confirmed-round"] + ", thank you!"
+      
+    },
+    //transaction validation, lifted from https://developer.algorand.org/docs/build-apps/hello_world/
+    async waitForConfirmation(algodClient, txId, timeout) {
+
+        const status = (await algodClient.status().do());
+        if (status === undefined) {
+          this.confirmationStatus = "Confirmation failed: Unable to get node status"
+        }
+
+        const startround = status["last-round"] + 1
+        let currentround = startround;
+
+        while (currentround < (startround + timeout)) {
+            const pendingInfo = await algodClient.pendingTransactionInformation(txId).do();
+            if (pendingInfo !== undefined) {
+                if (pendingInfo["confirmed-round"] !== null && pendingInfo["confirmed-round"] > 0) {
+                    //Got the completed Transaction
+                    return pendingInfo;
+                } else {
+                    if (pendingInfo["pool-error"] != null && pendingInfo["pool-error"].length > 0) {
+                        // If there was a pool error, then the transaction has been rejected!
+                        this.confirmationStatus = "Transaction " + txId + " rejected - pool error: " + pendingInfo["pool-error"];
+                    }
+                }
+            }
+            await algodClient.statusAfterBlock(currentround).do();
+            currentround++;
+        }
+
+        this.confirmationStatus = "Transaction " + txId + " not confirmed after " + timeout + " rounds!"
+    }
+  },
+}
+</script>
